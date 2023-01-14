73580d5581e08f9c28974ff1194bcb7b081e17ad
Doğan Işıklı <dogancetin2125@gmail.com>
2023-01-14 12:03:48 +0300

    Sınıf Geçme Durumu

--- SinifGecmeDurumu/src/SinifGecmeDurumu.java
+++ SinifGecmeDurumu/src/SinifGecmeDurumu.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 public class SinifGecmeDurumu {
     public static void main(String[] args) {
 
-      int Mat , Kimya , Fizik , Turkce , Bilisim;
+      int Mat , Kimya , Fizik , Turkce , Bilisim, maks;
+
       Scanner input = new Scanner(System.in);
 
-         System.out.print("Matematik Notunuz: ");
+
+        System.out.print("Matematik Notunuz: ");
          Mat = input.nextInt();
          System.out.print("Kimya Notunuz: ");
          Kimya = input.nextInt();
@@ -16,14 +18,20 @@ public class SinifGecmeDurumu {
          System.out.print("Bilişim Notunuz: ");
          Bilisim = input.nextInt();
 
+        if (Mat < 0 || Mat > 100 || Kimya < 0 || Kimya > 100 || Fizik < 0 || Fizik > 100
+                || Turkce < 0 || Turkce > 100 || Bilisim < 0 || Bilisim > 100)
+        {
+            System.out.println("Lütfen 0-100 aralığında sayı giriniz.");
+            return;
+        }
          double Ortalama = (Mat + Kimya + Fizik + Turkce + Bilisim) / 5;
 
-         if (55 >= Ortalama)
+        if (55 >= Ortalama)
          {
              System.out.println("Sınıfta Kaldınız!!!");
              System.out.println("Ortalamanız: " + Ortalama);
          }
-        else
+         else if (55<=Ortalama)
          {
              System.out.println("Sınıfı Geçtiniz...");
              System.out.println("Ortalamanız: " + Ortalama);
